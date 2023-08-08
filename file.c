67f60bf8e5741dd9251315bcf52b72fcca4b61a4
Sara Meixoeiro <sarameixoeiro@Puesto-10.local>
2023-08-08 12:28:39 +0200

    crallin

--- file.c
+++ file.c
@@ -6,7 +6,7 @@
 /*   By: smeixoei <smeixoei@student.42madrid.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/07/25 18:07:37 by smeixoei          #+#    #+#             */
-/*   Updated: 2023/08/07 17:14:09 by smeixoei         ###   ########.fr       */
+/*   Updated: 2023/08/08 12:27:48 by smeixoei         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -31,20 +31,21 @@ void	ft_check_file(t_game *data, char *map)
 	line = get_next_line(fd);
 	if (!line)
 		ft_error("Problems reading fd");
-	read = ft_strjoin(read, line);
 	data->map.y = 0;
 	data->map.x = ft_strlen(line) - 1;
-	free(line);
 	while (line)
 	{
-		line = get_next_line(fd);
 		read = ft_strjoin(read, line);
+		free(line);
+		line = get_next_line(fd);
 		printf("%s\n", read);
 		data->map.y++;
 	}
-	//data->map.chart = ft_split(read, '\n');
+	free(line);
+	data->map.chart = ft_split(read, '\n');
 	// int	i = 0;
 	// while (i < data->map.y)
 	// 	printf("%s\n", data->map.chart[i++]);
+	free(read);
 	close(fd);
 }
