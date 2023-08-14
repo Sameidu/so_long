3bcffcd3eb3833057336efd81fe94063f33f8014
Sara Meixoeiro <sarameixoeiro@Puesto-10.local>
2023-08-14 12:05:06 +0200

    jogfd

--- utils.c
+++ utils.c
@@ -6,7 +6,7 @@
 /*   By: smeixoei <smeixoei@student.42madrid.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/07/25 18:08:18 by smeixoei          #+#    #+#             */
-/*   Updated: 2023/08/14 11:11:59 by smeixoei         ###   ########.fr       */
+/*   Updated: 2023/08/14 12:04:50 by smeixoei         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,13 +18,63 @@ void	ft_route_map(t_game *data, int x, int y)
 		data->map.cp_chart[y][x] == 'P';
 }
 
+int	ft_route(t_game *data)
+{
+	int	y;
+	int	x;
+
+	y = 0;
+	while (y < data->map.y)
+	{
+		x = 0;
+		while (x < data->map.x)
+		{
+			if ((data->map.cp_chart[y][x] == 'E' && data->map.cp_chart[y][x + 1] == 'P')
+				|| (data->map.cp_chart[y][x] == 'E' && data->map.cp_chart[y][x - 1] == 'P')
+				|| (data->map.cp_chart[y][x] == 'E' && data->map.cp_chart[y + 1][x] == 'P')
+				|| (data->map.cp_chart[y][x] == 'E' && data->map.cp_chart[y - 1][x] == 'P'))
+				return (1);
+			x++;
+		}
+		y++;
+	}
+	ft_error("ERROR\nINVALID MAP");
+	return (0);
+}
+
+int	ft_check_coin(t_game *data)
+{
+	int	x;
+	int	y;
+
+	y = 0;
+	while (y < data->map.y)
+	{
+		x = 0;
+		while (x < data->map.x)
+		{
+			if ( data->map.cp_chart[y][x] == 'C')
+				ft_error("ERROR\nINVALID MAP");
+			x++;
+		}
+		y++;
+	}
+	return (1);
+}
+
 int	ft_exit(t_game *data, int x, int y)
 {
 	if (data->map.cp_chart[y][x] == 'P' &&
-		(data->map.cp_chart[y][x] == '0'
-		|| data->map.cp_chart[y][x] == 'C'
-		|| data->map.cp_chart[y][x] == '0'
-		|| data->map.cp_chart[y][x] == 'C'))
+		(data->map.cp_chart[y][x + 1] == '0'
+		|| data->map.cp_chart[y][x + 1] == 'C'
+		|| data->map.cp_chart[y][x - 1] == '0'
+		|| data->map.cp_chart[y][x - 1] == 'C'
+		|| data->map.cp_chart[y + 1][x] == '0'
+		|| data->map.cp_chart[y + 1][x] == 'C'
+		|| data->map.cp_chart[y - 1][x] == '0'
+		|| data->map.cp_chart[y - 1][x] == 'C'))
+		return (1);
+	return (0);
 }
 
 int    ft_flood_fill(t_game *data)
