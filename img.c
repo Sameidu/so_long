65a8aa51c12ede56422a7154e0c700d8d3f85a31
Sara Meixoeiro durante <smeixoei@c2r10s4.42madrid.com>
2023-09-21 19:03:28 +0200

    img pst

--- img.c
+++ img.c
@@ -17,34 +17,34 @@ void	ft_put_img(t_game *data, char value)
 	int	size;
 
 	size = SIZE;
-	ft_xpm_img(data);
+	printf("%d\n", value);
 	// if (value == '0')
 	// 	mlx_put_image_to_window(data->mlx, data->mlx_win, data->map.floor, size, size);
 	if (value == '1')
-		mlx_put_image_to_window(data->mlx, data->mlx_win, data->map.wall, size, size);
+		mlx_put_image_to_window(data->mlx, data->mlx_win, data->map.wall, 0, 0);
 	else if (value == 'C')
-		mlx_put_image_to_window(data->mlx, data->mlx_win, data->map.coin, size, size);
+		mlx_put_image_to_window(data->mlx, data->mlx_win, data->map.coin, 32, 0);
 	else if (value == 'E')
-		mlx_put_image_to_window(data->mlx, data->mlx_win, data->map.exit, size, size);
+		mlx_put_image_to_window(data->mlx, data->mlx_win, data->map.exit, 0, 32);
 	else if (value == 'P')
-		mlx_put_image_to_window(data->mlx, data->mlx_win, data->map.main, size, size);
+		mlx_put_image_to_window(data->mlx, data->mlx_win, data->map.main, 32, 32);
 }
 
 void	ft_xpm_img(t_game *data)
 {
 	data->map.coin = mlx_xpm_file_to_image(data->mlx, "./art/heart.xpm", &data->w, &data->h);
 	if (!data->map.coin)
-		ft_error("No access to a xpm file");
+		ft_error("No access to a xpm file 1");
 	data->map.exit = mlx_xpm_file_to_image(data->mlx, "./art/brain.xpm", &data->w, &data->h);
 	if (!data->map.exit)
-		ft_error("No access to a xpm file");
+		ft_error("No access to a xpm file 2");
 	data->map.wall = mlx_xpm_file_to_image(data->mlx, "./art/skull.xpm", &data->w, &data->h);
-	// if (!data->map.wall)
-	// 	ft_error("No access to a xpm file");
+	if (!data->map.wall)
+		ft_error("No access to a xpm file 3");
 	// data->map.floor = mlx_xpm_file_to_image(data->mlx, "./art/", &data->w, &data->h);
-	if (!data->map.floor)
-		ft_error("No access to a xpm file");
+	// if (!data->map.floor)
+	// 	ft_error("No access to a xpm file 3");
 	data->map.main = mlx_xpm_file_to_image(data->mlx, "./art/brain.xpm", &data->w, &data->h);
 	if (!data->map.main)
-		ft_error("No access to a xpm file");
+		ft_error("No access to a xpm file 4");
 }
