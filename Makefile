ebb5b90dc2fc616370c5bd8a85647456eef4c35c
Sameidu <sameidudu@gmail.com>
2024-01-17 21:30:48 +0100

    idk

--- Makefile
+++ Makefile
@@ -1,4 +1,4 @@
-##################        LINUX
+# #################        LINUX
 
 #  NAME		=	so_long
 #  CC			=	gcc
--- keys.c
+++ keys.c
@@ -22,6 +22,8 @@ void	ft_up(t_game *data)
 	if ((data->map.chart[y][x] == '0' || data->map.chart[y][x] == 'C')
 		&& data->map.chart[y][x] != '1')
 	{
+		if (data->map.chart[y][x] == 'C')
+			data->map.count_coin--;
 		data->map.chart[data->map.p_y][data->map.p_x] = '0';
 		data->map.p_x = x;
 		data->map.p_y = y;
@@ -40,7 +42,8 @@ void	ft_down(t_game *data)
 	if ((data->map.chart[y][x] == '0' || data->map.chart[y][x] == 'C')
 		&& data->map.chart[y][x] != '1')
 	{
-		printf("abajo");
+		if (data->map.chart[y][x] == 'C')
+			data->map.count_coin--;
 		data->map.chart[data->map.p_y][data->map.p_x] = '0';
 		data->map.p_x = x;
 		data->map.p_y = y;
@@ -59,6 +62,8 @@ void	ft_left(t_game *data)
 	if ((data->map.chart[y][x] == '0' || data->map.chart[y][x] == 'C')
 		&& data->map.chart[y][x] != '1')
 	{
+		if (data->map.chart[y][x] == 'C')
+			data->map.count_coin--;
 		data->map.chart[data->map.p_y][data->map.p_x] = '0';
 		data->map.p_x = x;
 		data->map.p_y = y;
@@ -77,6 +82,8 @@ void	ft_right(t_game *data)
 	if ((data->map.chart[y][x] == '0' || data->map.chart[y][x] == 'C')
 		&& data->map.chart[y][x] != '1')
 	{
+		if (data->map.chart[y][x] == 'C')
+			data->map.count_coin--;
 		data->map.chart[data->map.p_y][data->map.p_x] = '0';
 		ft_print_map(data, y, x);
 		data->map.p_x = x;
