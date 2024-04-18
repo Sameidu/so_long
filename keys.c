e0e847db3b14852cc9457d2f7d9a5325e4572879
Sara Meixoeiro <sarameixoeiro@Puesto-10.local>
2024-04-18 10:34:15 +0200

    stp

--- keys.c
+++ keys.c
@@ -32,6 +32,7 @@ void	ft_up(t_game *data)
 		data->map.p_y = ((data->map.p_x = x), y);
 		if (data->map.chart[data->map.p_y][data->map.p_x] != 'E')
             data->map.chart[data->map.p_y][data->map.p_x] = 'P';
+		data->map.steps++;
 		ft_print_map(data, data->map.p_y, data->map.p_x);
 	}
 }
@@ -56,6 +57,7 @@ void	ft_down(t_game *data)
 		data->map.p_y = ((data->map.p_x = x), y);
 		if (data->map.chart[data->map.p_y][data->map.p_x] != 'E')
            data->map.chart[data->map.p_y][data->map.p_x] = 'P';
+		data->map.steps++;
 		ft_print_map(data, data->map.p_y, data->map.p_x);
 	}
 }
@@ -80,6 +82,7 @@ void	ft_left(t_game *data)
 		data->map.p_x = ((data->map.p_y = y), x);
 		if (data->map.chart[data->map.p_y][data->map.p_x] != 'E')
             data->map.chart[data->map.p_y][data->map.p_x] = 'P';
+		data->map.steps++;
 		ft_print_map(data, data->map.p_y, data->map.p_x);
 	}
 }
@@ -104,6 +107,7 @@ void	ft_right(t_game *data)
 		data->map.p_x = ((data->map.p_y = y), x);
 		if (data->map.chart[data->map.p_y][data->map.p_x] != 'E')
             data->map.chart[data->map.p_y][data->map.p_x] = 'P';
+		data->map.steps++;
 		ft_print_map(data, data->map.p_y, data->map.p_x);
 	}
 }
@@ -123,6 +127,5 @@ int	ft_read_key(int key, t_game *data)
 		ft_right(data);
 	ft_putnbr_fd(data->map.steps, 1);
 	ft_putstr_fd("\n", 1);
-	data->map.steps++;
 	return (0);
 }
